c9490cf0ba3c8280794fbe181772e1546bf211c3
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-04-22 00:17:40 +0900

    Modified to Serve HTTPS and HTTP

--- main.go
+++ main.go
@@ -1,14 +1,122 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"gitlab.com/kitalabs/go-2gaijin/router"
+	"golang.org/x/crypto/acme/autocert"
 )
+
+const (
+	htmlIndex = `<html><body>Welcome!</body></html>`
+	httpPort  = "127.0.0.1:8080"
+)
+
+var (
+	flgProduction          = false
+	flgRedirectHTTPToHTTPS = false
+)
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, htmlIndex)
+}
+
+func makeServerFromGin(gin *gin.Engine) *http.Server {
+	// set timeouts so that a slow or malicious client doesn't
+	// hold resources forever
+	return &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      gin,
+	}
+}
+
+func makeHTTPServer() *http.Server {
+	gin := router.Router()
+	return makeServerFromGin(gin)
+
+}
+
+func makeHTTPToHTTPSRedirectServer() *http.Server {
+	handleRedirect := func(c *gin.Context) {
+		r := c.Request
+		w := c.Writer
+		newURI := "https://" + r.Host + r.URL.String()
+		http.Redirect(w, r, newURI, http.StatusFound)
+	}
+	gin := router.Router()
+	gin.GET("/", handleRedirect)
+	return makeServerFromGin(gin)
+}
+
+func parseFlags() {
+	flag.BoolVar(&flgProduction, "production", false, "if true, we start HTTPS server")
+	flag.BoolVar(&flgRedirectHTTPToHTTPS, "redirect-to-https", false, "if true, we redirect HTTP to HTTPS")
+	flag.Parse()
+}
 
 func main() {
 
-	router := router.Router()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	var httpsSrv *http.Server
+	var m *autocert.Manager
+
+	// when testing locally it doesn't make sense to start
+	// HTTPS server, so only do it in production.
+	// In real code, I control this with -production cmd-line flag
+	if flgProduction {
+		// Note: use a sensible value for data directory
+		// this is where cached certificates are stored
+		dataDir := "."
+		hostPolicy := func(ctx context.Context, host string) error {
+			// Note: change to your real domain
+			allowedHost := "go.2gaijin.com"
+			if host == allowedHost {
+				return nil
+			}
+			return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
+		}
+
+		httpsSrv = makeHTTPServer()
+		m := &autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: hostPolicy,
+			Cache:      autocert.DirCache(dataDir),
+		}
+		httpsSrv.Addr = ":443"
+		httpsSrv.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+
+		go func() {
+			err := httpsSrv.ListenAndServeTLS("", "")
+			if err != nil {
+				log.Fatalf("httpsSrv.ListendAndServeTLS() failed with %s", err)
+			}
+		}()
+	}
+
+	var httpSrv *http.Server
+	if flgRedirectHTTPToHTTPS {
+		httpSrv = makeHTTPToHTTPSRedirectServer()
+	} else {
+		httpSrv = makeHTTPServer()
+	}
+	// allow autocert handle Let's Encrypt callbacks over http
+	if m != nil {
+		httpSrv.Handler = m.HTTPHandler(httpSrv.Handler)
+	}
+
+	httpSrv.Addr = httpPort
+	fmt.Printf("Starting HTTP server on %s\n", httpPort)
+	err := httpSrv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("httpSrv.ListenAndServe() failed with %s", err)
+	}
 }
