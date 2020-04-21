b2c076294ab94c85b622778fb11145129ea3ac38
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-04-21 14:12:53 +0900

    Changed Product Card Structure

--- models/product.go
+++ models/product.go
@@ -46,6 +46,7 @@ type ProductCard struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name       string             `json:"name" bson:"name"`
 	Price      int                `json:"price" bson:"price"`
+	User       primitive.ObjectID `bson:"user_id"`
 	SellerName string             `json:"seller_name"`
 	Loc        string             `json:"loc"`
 	ImgURL     string             `json:"img_url"`
