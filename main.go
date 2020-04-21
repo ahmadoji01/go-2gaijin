a5c77e54cf9a9d90bcb071c41a7f57ea10bfc4dd
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-04-22 00:14:47 +0900

    Modified to Serve HTTP

--- main.go
+++ main.go
@@ -10,8 +10,5 @@ import (
 func main() {
 
 	router := router.Router()
-	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", router)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
