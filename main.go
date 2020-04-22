98206be28c416f8f0f655f08d7d2ceaeb9d7e787
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-04-22 12:21:19 +0900

    Added Redirect HTTP to HTTPS

--- main.go
+++ main.go
@@ -7,9 +7,21 @@ import (
 	"gitlab.com/kitalabs/go-2gaijin/router"
 )
 
+const (
+	domainName = "go.2gaijin.com"
+)
+
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://"+domainName+":443"+r.RequestURI, http.StatusMovedPermanently)
+}
+
 func main() {
 
 	router := router.Router()
-	//log.Fatal(http.ListenAndServe(":80", router))
+	go func() {
+		if err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)); err != nil {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
 	log.Fatal(http.ListenAndServeTLS(":443", "keys/cert.pem", "keys/key.pem", router))
 }
