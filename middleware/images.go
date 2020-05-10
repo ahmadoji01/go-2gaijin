b2104dcf4c8d83ccc07eeeb90a099996bf319387
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-05-10 20:17:40 +0900

    Adjusted Chat Lobby Response #3

---
+++ middleware/images.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//Image URL Prefix
+var ImgURLPrefix string = "https://storage.googleapis.com/rails-2gaijin-storage/"
+
+func FindProductImages(productID primitive.ObjectID) []interface{} {
+	var results []interface{}
+
+	coll := DB.Collection("product_images")
+	cur, err := coll.Find(context.Background(), bson.D{{"product_id", productID}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result = struct {
+		ID    primitive.ObjectID `json:"_id" bson:"_id"`
+		Image string             `json:"img_url" bson:"image"`
+	}{}
+
+	for cur.Next(context.Background()) {
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		result.Image = ImgURLPrefix + "uploads/product_image/image/" + result.ID.Hex() + "/" + result.Image
+
+		results = append(results, result)
+	}
+	return results
+}
+
+func FindUserAvatar(userID primitive.ObjectID, avatarName string) string {
+	if avatarName == "" {
+		return ""
+	} else {
+		var avatarURL = ImgURLPrefix + "uploads/user/avatar/" + userID.Hex() + "/" + avatarName
+		return avatarURL
+	}
+}
