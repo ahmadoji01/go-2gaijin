d20c93822f4519191347e54f70ca4bb6cd04ff22
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-05-11 17:04:11 +0900

    Small Bug Fix #3

--- responses/jsonresponse.go
+++ responses/jsonresponse.go
@@ -116,5 +116,5 @@ type ChatRoomData struct {
 type GenericResponse struct {
 	Status  string      `json:"status"`
 	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
+	Data    interface{} `json:"data"`
 }
