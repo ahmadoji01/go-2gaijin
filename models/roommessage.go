240fe718e62728e868b569ecfb80d1020bbec659
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-06-25 13:43:41 +0900

    Adjusted Chat Response #

--- models/roommessage.go
+++ models/roommessage.go
@@ -6,10 +6,10 @@ type RoomMessage struct {
 	ID        primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
 	Message   string               `json:"message" bson:"message"`
 	Name      string               `json:"name,omitempty" bson:"name,omitempty"`
-	Image     string               `json:"image,omitempty" bson:"image,omitempty"`
+	Image     string               `json:"image" bson:"image"`
 	ImgData   string               `json:"img_data,omitempty"`
-	CreatedAt primitive.DateTime   `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UserID    primitive.ObjectID   `json:"user_id,omitempty" bson:"user_id,omitempty"`
-	RoomID    primitive.ObjectID   `json:"room_id,omitempty" bson:"room_id,omitempty"`
+	CreatedAt primitive.DateTime   `json:"created_at" bson:"created_at"`
+	UserID    primitive.ObjectID   `json:"user_id" bson:"user_id"`
+	RoomID    primitive.ObjectID   `json:"room_id" bson:"room_id"`
 	ReaderIDs []primitive.ObjectID `json:"reader_ids,omitempty" bson:"reader_ids,omitempty"`
 }
