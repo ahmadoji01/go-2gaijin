66b4c0485adf1dc0091d86cbae0a90c724905a86
Ahmad Ridwan Fauzi <admin@kitalabs.com>
2020-08-18 20:21:27 +0900

    Changed Config for Desktop Web App Rest

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ const (
 	MobileWebAppLink  = "https://beta06012020.2gaijin.com/#!/"
 	AndroidAppLink    = "https://beta06012020.2gaijin.com/#!/"
 	IOSAppLink        = "https://beta06012020.2gaijin.com/#!/"
-	DesktopWebAppLink = "https://beta06012020.2gaijin.com/#!/"
+	DesktopWebAppLink = "https://webbeta06012020.2gaijin.com/"
 	OmisePublicKey    = "pkey_test_5kg29vvml1oqwpclafj"
 	OmiseSecretKey    = "skey_test_5kg0zfzligi38ntyog5"
 	SubsPerMonth      = 100
